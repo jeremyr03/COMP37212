70fce977e91c95734bc1e73b76f85903db51a394
t11915jr <jeremy.roe@student.manchester.ac.uk>
2023-03-17 15:54:21 +0000

    Finished cwk2

--- cwk-2/main2.0.py
+++ cwk-2/main2.0.py
@@ -1,7 +1,5 @@
 import math
 import sys
-from os.path import isfile, join
-
 import cv2
 import os
 import numpy as np
@@ -12,14 +10,14 @@ from matplotlib import pyplot as plt
 
 def getImage(i):
     # Display Image
-    print(i)
     image = cv2.imread(i)
     print(f"trying to open: {i}")
+
     if image is None:
         print(f'Error: failed to open {i}')
         sys.exit()
 
-    PATH = os.path.dirname(os.path.abspath(i))
+    PATH = f"./{os.path.dirname(os.path.relpath(i))}/"
     FILENAME = os.path.splitext(os.path.basename(i))[0]
     EXTENSION = os.path.splitext(os.path.basename(i))[1]
 
@@ -28,6 +26,7 @@ def getImage(i):
     print(f"Finding points for: {i}")
     image_points = harrisPointsDetector(image, PATH, FILENAME, EXTENSION)
     print(f"Found points for: {i}")
+
     print(f"Finding features for: {i}")
     image_descriptors, image_features = featureDescription(image, image_points, PATH, FILENAME, EXTENSION)
     print(f"Found features for: {i}")
@@ -54,14 +53,21 @@ def sobel(image, PATH, FILENAME, EXTENSION):
     return [grad_x, grad_y]
 
 
-def interestPoint(harris):
+def interestPoint(harris, PATH, FILENAME, EXTENSION):
     # threshold
     local_maxima = scipy.ndimage.maximum_filter(harris,
                                                 size=(7, 7),
                                                 mode="reflect")
-    print(np.max(local_maxima))
-    threshold_value = np.percentile(local_maxima, 99)
-    _, local_maxima = cv2.threshold(local_maxima, threshold_value, 255, cv2.THRESH_BINARY)  # threshold value 5000000
+
+    threshold_value = np.percentile(local_maxima, 99.9)
+
+    v, local_maxima = cv2.threshold(local_maxima,
+                                    threshold_value,
+                                    255,
+                                    cv2.THRESH_BINARY)  # threshold value 5000000
+
+    thresholds[0].append(v)
+    thresholds[1].append(len(local_maxima))
 
     # non-local maxima suppression
     for x_index, row in enumerate(local_maxima):
@@ -76,12 +82,13 @@ def interestPoint(harris):
                     for i in range(left, right):
                         for j in range(top, bottom):
                             # remove neighbouring points
-                            if int(local_maxima[i][j]) >= 255 and i != x_index and j != y_index:
+                            if int(local_maxima[i][j]) >= 255 \
+                                    and i != x_index \
+                                    and j != y_index:
                                 local_maxima[i][j] = 0.0
             except IndexError:
                 continue
-    # threshold_image = cv2.adaptiveThreshold(harris, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 7, 2)
-    # cv2.imwrite(f"{PATH}threshold_{FILENAME}.{EXTENSION}", threshold_image)
+
     return local_maxima
 
 
@@ -91,7 +98,7 @@ def harrisPointsDetector(image, PATH, FILENAME, EXTENSION):
     cv2.imwrite(f"{PATH}{RESULTS_DIR}greyscale_{FILENAME}{EXTENSION}", grey_image)
 
     # blur image to remove noise
-    blurred_image = cv2.blur(grey_image, (5, 5))
+    blurred_image = cv2.GaussianBlur(grey_image, (5, 5), 0.5)
     cv2.imwrite(f"{PATH}{RESULTS_DIR}blurred_{FILENAME}{EXTENSION}", blurred_image)
 
     # Spatial derivative calculation (sobel edge detection) -> returns an array [x, y]
@@ -102,10 +109,12 @@ def harrisPointsDetector(image, PATH, FILENAME, EXTENSION):
                                         sigma=0.5,
                                         mode="reflect",
                                         radius=2)
+
     Iyy = scipy.ndimage.gaussian_filter(sobel_images[1] ** 2,
                                         sigma=0.5,
                                         mode="reflect",
                                         radius=2)
+
     Ixy = scipy.ndimage.gaussian_filter(sobel_images[0] * sobel_images[1],
                                         sigma=0.5,
                                         mode="reflect",
@@ -117,13 +126,15 @@ def harrisPointsDetector(image, PATH, FILENAME, EXTENSION):
 
     r = determinant - (0.05 * trace ** 2)
 
-    # supress non-local maxima ?
-    maxima = interestPoint(r)
+    # supress non-local maxima
+    maxima = interestPoint(r, PATH, FILENAME, EXTENSION)
 
     points = []
+
     # turn points into key points
     for row_index, row in enumerate(maxima):
         for column_index, p in enumerate(row):
+
             if p != 0:
                 points.append(cv2.KeyPoint(x=column_index, y=row_index, size=1.0))
 
@@ -134,64 +145,63 @@ def featureDescription(image, points, PATH, FILENAME, EXTENSION):
     # Initiate ORB detector
     orb = cv2.ORB_create()
 
-    # testing purpose
-    kp = orb.detect(image, None)
-    kp, des = orb.compute(image, kp)
-
     # compute the descriptors with ORB
-    # kp, des = orb.compute(image, points)
-    # print(f"{FILENAME} kp: {kp} des:{des}\n {points}")
+    kp, des = orb.compute(image, points)
 
-    # draw only keypoints location,not size and orientation
+    # draw only key points location, not size and orientation
     new_image = cv2.drawKeypoints(image, kp, None, color=(0, 255, 0), flags=0)
 
     cv2.imwrite(f"{PATH}{RESULTS_DIR}feature_description_{FILENAME}{EXTENSION}", new_image)
-    # cv2.imshow(f"feature_description_{FILENAME}", new_image)
+    cv2.imshow(f"feature_description_{FILENAME}", new_image)
 
     return np.array(des), np.array(kp)
 
 
 def ssd(image1, image2):
     print("SSD")
+
     best_matches = [cv2.DMatch() for _ in range(len(image1['descriptors']))]
     distance = scipy.spatial.distance.cdist(image1["descriptors"], image2["descriptors"])
-    best_distance = float("inf")
+
     for index, val in enumerate(distance):
+
         best_matches[index].queryIdx = index
         best_matches[index].trainIdx = numpy.ma.argmin(val)
         best_matches[index].distance = val[numpy.ma.argmin(val)]
+
     return best_matches
 
 
 def ratio(image1, image2):
     print("ratio")
+
     best_matches = [cv2.DMatch() for _ in range(len(image1['descriptors']))]
-    print(f'descriptors: image 1{image2["descriptors"]} and image2{image2["descriptors"]}')
     distance = scipy.spatial.distance.cdist(image1["descriptors"], image2["descriptors"])
+
     for index, val in enumerate(distance):
+
         smallest_index = numpy.ma.argmin(val)
         smallest = val[smallest_index]
         temp = val.copy()
         numpy.delete(temp, smallest_index)
         second_smallest_index = numpy.ma.argmin(temp)
         second_smallest = temp[second_smallest_index]
+
         best_matches[index].queryIdx = index
         best_matches[index].trainIdx = numpy.ma.argmin(val)
         best_matches[index].distance = smallest / second_smallest
+
     return best_matches
 
 
 def matchFeatures(image1, image2, matching=ratio):
-    # euclidian distance
-    # print(f"lengths: {len(image1['descriptors'])}, {len(image2['descriptors'])}")
     print(f"feature matching: {image1['filename']} and {image2['filename']}")
 
     match = matching(image1, image2)
-    match = [valid for valid in match if not(math.isnan(valid.distance))]
+    match = [valid for valid in match if not (math.isnan(valid.distance))]
     match.sort(key=lambda x: x.distance)
-    # for i in range(50):
-    #     print(match[i].distance, type(match[i].distance))
 
+    # shows first 50 matches
     matches_image = cv2.drawMatches(image1['image'],
                                     image1['features'],
                                     image2['image'],
@@ -202,36 +212,33 @@ def matchFeatures(image1, image2, matching=ratio):
     cv2.imwrite(f"{image1['path']}{RESULTS_DIR}{image1['filename']}_matching_{image2['filename']}.{image1['ext']}",
                 matches_image)
 
-    # best match has the smallest distance
-    # numpy.ma.argmin(i)
-
     return matches_image
-    # print(match)
+
+
+def histogram(i, name="./untitled.jpg"):
+    hist = cv2.calcHist([i], [0], None, [256], [0, 256])
+    hist = hist.reshape(256)
+
+    # Plot histogram
+    plt.bar(np.linspace(0, 255, 256), hist)
+    plt.title(f'Histogram for {name}')
+    plt.ylabel('Frequency')
+    plt.xlabel('Grey Level')
+    plt.show()
+    plt.savefig(f'{name}')
 
 
 SAMPLE_IMAGE = "./images/bernieSanders.jpg"
 RESULTS_DIR = "/results/"
 K = 0.05
 
-print(len(sys.argv))
-# "./images/bernieSanders.jpg"
 if len(sys.argv) == 1:
     image_1 = SAMPLE_IMAGE
     img1 = getImage(image_1)
     sys.exit()
 
-# elif len(sys.argv) == 2:
-#     test_images = []
-#     for (dirpath, dirnames, filenames) in os.walk(f"{sys.argv[1]}/samples/"):
-#         for file in filenames:
-#             if isfile(join(dirpath, file)) and (os.path.splitext(file)[1] in (".jpg", ".png", "jpeg")):
-#                 test_images.append(getImage(join(dirpath, file)))
-#
-#     print(test_images)
-#     img1 = test_images[0]
-#     test_images.pop(0)
-
 elif len(sys.argv) >= 3:
+    thresholds = [[], []]
     test_images = []
     img1 = getImage(sys.argv[1])
     for im in range(2, len(sys.argv)):
@@ -244,5 +251,10 @@ else:
 
 for img in test_images:
     m = matchFeatures(img1, img)
+
+plt.plot(thresholds[0], thresholds[1], 'o')
+plt.xlabel("Threshold Value")
+plt.ylabel("Number of Interest Points")
+plt.show()
 
 cv2.destroyAllWindows()
