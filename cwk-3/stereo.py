82b65d5b33e3db403d58b0dd908ea89a92da52b5
t11915jr <jeremy.roe@student.manchester.ac.uk>
2023-05-04 13:54:24 +0100

    Task 1: - Focal Length calculated - Disparity Map calculated - Views of Scene unfinished

--- cwk-3/stereo.py
+++ cwk-3/stereo.py
@@ -12,18 +12,32 @@ ratio = 3
 kernel_size = 3
 canny_vals = [90, 158]
 
+underline = "________________________________________________"
+
 cam0 = [(5806.559, 0, 1429.219),
         (0, 5806.559, 993.403),
         (0, 0, 1)]
 cam1 = [(5806.559, 0, 1543.51),
         (0, 5806.559, 993.403),
         (0, 0, 1)]
+sensor = [22.2, 14.8]
+resolution = [3088, 2056]
 doffs = 114.291
 baseline = 174.019
-width = 2960
-height = 2016
+# width = 2960
+# height = 2016
+width = 740
+height = 505
 
 img = [f"./img/input/umbrella{i}.png" for i in ('L', 'R')]
+
+
+def pause():
+    while True:
+        if cv2.waitKey(1) == ord(' '):
+            break
+
+    cv2.destroyAllWindows()
 
 
 def open_image(i=None):
@@ -41,58 +55,55 @@ def open_image(i=None):
     return image
 
 
-def canny():
+def getDisparity():
+    global disparityNormalised
     canny_images = [cv2.Canny(i, canny_vals[0], canny_vals[1]) for i in images]
     disparity_img = disparity.getDisparityMap(canny_images[0], canny_images[1], 64, 5)
     disparityNormalised = np.interp(disparity_img, (disparity_img.min(), disparity_img.max()), (0.0, 1.0))
     cv2.imshow('Disparity', disparityNormalised)
-    cv2.imwrite(f'./img/output/Disparity.png', disparityNormalised)
-
-
-def sobel():
-    gradients = [
-        (cv2.convertScaleAbs(
-            cv2.Sobel(cv2.GaussianBlur(i, (3, 3), cv2.BORDER_DEFAULT),
-                      cv2.CV_16S,
-                      1, 0, ksize=3, scale=1, delta=0,
-                      borderType=cv2.BORDER_DEFAULT)),
-         cv2.convertScaleAbs(
-             cv2.Sobel(cv2.GaussianBlur(i, (3, 3), cv2.BORDER_DEFAULT),
-                       cv2.CV_16S,
-                       0, 1, ksize=3, scale=1, delta=0,
-                       borderType=cv2.BORDER_DEFAULT)))
-        for i in images
-    ]
-
-    return [cv2.addWeighted(i[0], 0.5, i[1], 0.5, 0) for i in gradients]
+    picture = np.interp(disparity_img, (disparity_img.min(), disparity_img.max()), (0.0, 255.0))
+    cv2.imwrite(f'./img/output/Disparity.png', picture)
 
 
 def change1(x):
     canny_vals[0] = x
-    canny()
+    getDisparity()
 
 
 def change2(x):
     canny_vals[1] = x
-    canny()
+    getDisparity()
 
 
 images = [open_image(img[0]), open_image(img[1])]
 cv2.imshow(f"blur", images[0])
 
 # FOCAL LENGTH CALCULATION
-
+# focal length(mm) = focal length (px) / (sensor width (mm) x image width (px))
+focal_length = cam0[0][0] * (sensor[0] / resolution[0])
+print(f"focal length "
+      f"= focal length ({cam0[0][0]}px) * (sensor width ({sensor[0]}mm) / image width ({resolution[0]}mm))"
+      f"\nf={focal_length}mm"
+      f"\n{underline}")
 
 # DISPARITY MAP
-# sobel_images = sobel()
 cv2.imshow(f"Disparity", images[0])
+disparityNormalised = []
 cv2.createTrackbar('Canny1', 'Disparity', canny_vals[0], 255, change1)
 cv2.createTrackbar('Canny2', 'Disparity', canny_vals[1], 255, change2)
-change1(canny_vals[0])
-change2(canny_vals[1])
 
-while True:
-    if cv2.waitKey(1) == ord(' '):
-        break
+pause()
 
-cv2.destroyAllWindows()
+# VIEWS OF THE SCENE
+# depth = baseline*(focal_length/(disparity + doffs))
+image_depth = np.zeros_like(disparityNormalised)
+
+# calculate Z value for each coordinate
+for i, row in enumerate(disparityNormalised):
+    for j, column in enumerate(row):
+        image_depth[i][j] = baseline*(focal_length/(disparityNormalised[i][j] + doffs))
+
+# Calculate world co-ordinates using similar triangles
+
+# Plot disparity
+disparity.plot(disparityNormalised)
